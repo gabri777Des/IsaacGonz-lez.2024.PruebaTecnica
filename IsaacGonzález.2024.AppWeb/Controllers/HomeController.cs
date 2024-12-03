f716a92c7751a935f40ff491e9c44b2c7cbf0598
gabri777Des <gabrisac2006@gmail.com>
2024-12-03 12:43:29 -0600

    Agregar archivos de proyecto.

---
+++ IsaacGonzález.2024.AppWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using IsaacGonzález._2024.AppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IsaacGonzález._2024.AppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
